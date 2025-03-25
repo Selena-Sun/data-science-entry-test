21d41c25f9a9dfc455897508e694317bebfa3f4f
Selena-Sun <168087863+Selena-Sun@users.noreply.github.com>
2025-03-25 17:55:31 +0800

    Create p7.py

---
+++ p7.py
@@ -0,0 +1,15 @@
+class Car:
+
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def describe_car(self):
+        print(f"{self.year} {self.make} {self.model}")
+
+# Task 2: Create an instance of the Car class
+my_car = Car("Toyota", "Corolla", 2020)
+
+# Call the describe_car method
+my_car.describe_car()
