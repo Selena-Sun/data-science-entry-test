16ad6c73ea5e2b4c503e0c5577353dfcb91c223b
Selena-Sun <168087863+Selena-Sun@users.noreply.github.com>
2025-03-25 18:04:32 +0800

    Update p2.py

--- p2.py
+++ p2.py
@@ -4,9 +4,10 @@ def find_and_replace(lst, find_val, replace_val):
     for index in range(len(lst)):
         if lst[index] == find_val:
             lst[index] = replace_val
-            
+
             return lst
 
+# Scenario 1:
 original_list = [1, 2, 3, 4, 2, 2]
 find_value = 2
 replace_value = 5
@@ -14,6 +15,7 @@ replace_value = 5
 modified_list = find_and_replace(original_list, find_value, replace_value)
 print(modified_list)
 
+# Scenario 2:
 original_list = ["apple", "banana", "apple"]
 find_value = "apple"
 replace_value = "orange"
