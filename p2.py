90c1ee52c9438c43c730a7f92ce866c7e6dd2507
Selena-Sun <168087863+Selena-Sun@users.noreply.github.com>
2025-03-25 16:58:37 +0800

    Create p2.py

---
+++ p2.py
@@ -0,0 +1,22 @@
+def find_and_replace(lst, find_val, replace_val):
+    if not isinstance(lst, list):
+    	print("Input must be a list.")
+    for index in range(len(lst)):
+        if lst[index] == find_val:
+            lst[index] = replace_val
+            
+            return lst
+
+original_list = [1, 2, 3, 4, 2, 2]
+find_value = 2
+replace_value = 5
+
+modified_list = find_and_replace(original_list, find_value, replace_value)
+print(modified_list)
+
+original_list = ["apple", "banana", "apple"]
+find_value = "apple"
+replace_value = "orange"
+
+modified_list = find_and_replace(original_list, find_value, replace_value)
+print(modified_list)
